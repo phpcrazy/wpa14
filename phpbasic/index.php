78c96c4bbf37f6e3a745e2107ed172fdc066febc
myanmarlinks <myanmarlinks.net@gmail.com>
2014-03-21 19:17:38 +0630

    Initial Commit

---
+++ phpbasic/index.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+var_dump($GLOBALS);
+var_dump($_GET); // $GLOBALS['_GET']
+var_dump($_POST);
+var_dump($_COOKIE);
+var_dump($_FILES);
+var_dump($_REQUEST);
+var_dump($_SERVER);
+var_dump($_ENV);
+*/
+
+/*
+
+7 parts of GLOBALS REQUEST ARRAY
+1. _GET - URL Request
+/ 2. _POST - Header Request
+/ 3. _FILES - File Uploading Request
+===============
+6. _REQUEST - Collactive Request
+
+4. _COOKIE - User State / SESSION - Server State
+5. _ENV -
+
+7- _SERVER - User Agent + Server Data
+
+URI Request
+*/
+
+ob_start();
+echo "Hello World!";
+echo "How are you?";
+$temp = ob_get_clean();
+echo $temp;
+
+ ?>
